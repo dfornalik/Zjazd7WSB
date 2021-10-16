860d6fcf12f6dc4c1c438d85a44934ec59efc9c1
Daniel Fornalik <dfornalik@gmail.com>
2021-10-16 13:52:48 +0200

    Zamkniecie przegladarki, dodanie

---
+++ Object Repository/Page_Gazeta.pl - Polska i wiat - wiadomoci _e631e7/div_Post-Covid moe wystpowa u osb, ktre ago_b4ba97.rs
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Post-Covid moe wystpowa u osb, ktre ago_b4ba97</name>
+   <tag></tag>
+   <elementGuidId>da465c7d-c84d-42aa-b707-6f89e6ead3a4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//a[@id='LinkArea:BoxOpLink']/div[2])[14]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>timeline__linkTitle</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>&quot;Post-Covid może występować u osób, które łagodnie przeszły zakażenie&quot;</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;is-desktop&quot;]/main[@class=&quot;l-main&quot;]/section[@class=&quot;c-main-content&quot;]/div[@class=&quot;sectionTiles sectionTiles--opening sectionTiles--bigPlayer ad1Col--1 ad1Row--1 ad2Col--1 ad2Row--3&quot;]/div[@class=&quot;sectionTiles__box&quot;]/div[@class=&quot;timeline&quot;]/div[@class=&quot;timeline__list&quot;]/div[@class=&quot;timeline__box&quot;]/a[@id=&quot;LinkArea:BoxOpLink&quot;]/div[@class=&quot;timeline__linkTitle&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>(//a[@id='LinkArea:BoxOpLink']/div[2])[14]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Była gwiazda ekstraklasy robi furorę za granicą. Kibice go pokochali [WIDEO]'])[1]/following::div[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Adele udzieliła wywiadu na Zoomie. Fani zwracają uwagę na przedmiot w tle'])[1]/following::div[4]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Teoria czterech jeźdźców. Sprawdź, czy wasz związek jest udany i trwały'])[1]/preceding::div[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Uchodźcy na granicy z Białorusią. Prymas Polski wspiera apel medyków'])[1]/preceding::div[4]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='&quot;Post-Covid może występować u osób, które łagodnie przeszły zakażenie&quot;']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a[14]/div[2]</value>
+   </webElementXpaths>
+</WebElementEntity>
